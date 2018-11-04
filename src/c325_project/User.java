ade624e3442f67e2b5906a9c65b48077c09bf87a
Rachel <38117365+rrettig98@users.noreply.github.com>
2018-11-04 13:01:03 -0500

    created first GUI

---
+++ src/c325_project/User.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package c325_project;
+
+/**
+ *
+ * @author gitte
+ */
+public class User {
+
+    //gets and sets
+    private String userID;
+
+    public void setuserID(String id) {
+        userID = id;
+    }
+
+    public String getuserID() {
+        return userID;
+    }
+
+    private String firstName;
+
+    public void setfirstName(String fn) {
+        firstName = fn;
+    }
+
+    public String getfirstName() {
+        return firstName;
+    }
+
+    private String lastName;
+
+    public void setlastName(String ln) {
+        lastName = ln;
+    }
+
+    public String getlastName() {
+        return lastName;
+    }
+
+}
