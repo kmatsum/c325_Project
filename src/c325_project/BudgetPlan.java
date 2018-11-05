05fb0962e4f9a593dde39ae0064ce4a465f1515a
PaulTheNarwhal <muellerindy2@yahoo.com>
2018-11-04 21:15:48 -0500

    Added Service Class

--- src/c325_project/BudgetPlan.java
+++ src/c325_project/BudgetPlan.java
@@ -10,6 +10,8 @@ package c325_project;
  * @author Paul Mueller
  */
 public class BudgetPlan {
+
+    //GET AND SETS =============================================================
     private double homeExpenses;
 
     public void setbudgetPlan(double he) {
@@ -19,14 +21,64 @@ public class BudgetPlan {
     public double getbudgetPlan() {
         return homeExpenses;
     }
-    
+
     private double transportation;
 
-    public void setuserID(double tp) {
+    public void settransportation(double tp) {
         transportation = tp;
     }
 
-    public double getuserID() {
-        return homeExpenses;
+    public double gettransportation() {
+        return transportation;
+    }
+
+    private double health;
+
+    public void sethealth(double ha) {
+        health = ha;
+    }
+
+    public double gethealth() {
+        return health;
+    }
+
+    private double charity;
+
+    public void setcharity(double ch) {
+        charity = ch;
+    }
+
+    public double getcharity() {
+        return charity;
+    }
+
+    private double dailyLiving;
+
+    public void setdailyLiving(double dl) {
+        dailyLiving = dl;
+    }
+
+    public double getdailyLiving() {
+        return dailyLiving;
+    }
+
+    private double entertainment;
+
+    public void setentertainment(double et) {
+        entertainment = et;
+    }
+
+    public double getentertainment() {
+        return entertainment;
+    }
+
+    private double financial;
+
+    public void setfinancial(double fn) {
+        financial = fn;
+    }
+
+    public double getfinancial() {
+        return financial;
     }
 }
