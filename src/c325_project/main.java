3bfe76b9f54b176b05b6ae4a7745e75f60dadd2f
kazushi1712 <44615506+kazushi1712@users.noreply.github.com>
2018-11-04 20:43:56 -0500

    Formatted some stuff

--- src/c325_project/main.java
+++ src/c325_project/main.java
@@ -4,6 +4,7 @@ public class main {
 
     public static void main(String[] args) {
 
+        //OPENING FIRST FORM ===================================================
         MainScreen MainScreenWindow = new MainScreen();
         MainScreenWindow.setVisible(true);
         
