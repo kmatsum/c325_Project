1485dc5eb0c2cb6746a176f562ed52e70647551a
Clark462 <44757858+Clark462@users.noreply.github.com>
2018-11-04 20:11:19 -0500

    Added Account class

---
+++ src/c325_project/Account.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package c325_project;
+
+/**
+ *
+ * @author Jon
+ */
+public class Account {
+
+    String acctName;
+    Double balance;
+    String type;
+
+    public String getacctName() {
+        return acctName;
+    }
+
+    public void setacctName(String xacctName) {
+        xacctName = acctName;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double xbalance) {
+        xbalance = balance;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String xtype) {
+        xtype = type;
+    }
+}
