dbbd38d4242e41416a712e18dd45d1d188f18d6f
PaulTheNarwhal <muellerindy2@yahoo.com>
2018-11-04 21:28:14 -0500

    Added Pantry Class

---
+++ src/c325_project/Pantry.java
@@ -0,0 +1,14 @@
+package c325_project;
+
+public class Pantry {
+
+    private boolean perishable;
+
+    public void setperishable(boolean pe) {
+        perishable = pe;
+    }
+
+    public boolean getperishable() {
+        return perishable;
+    }
+}
