18bfb746eb292fbe36271f94e14ed72c9d7981d6
Rachel <38117365+rrettig98@users.noreply.github.com>
2018-11-04 21:26:47 -0500

    Merge origin/master into RachelGUIedits
    Conflicts:
    	src/c325_project/main.java

--- src/c325_project/main.java
+++ src/c325_project/main.java
@@ -2,12 +2,14 @@ package c325_project;
 
 public class main {
 
+    public static User user1 = new User();
+
     public static void main(String[] args) {
 
         //OPENING FIRST FORM ===================================================
         MainScreen MainScreenWindow = new MainScreen();
         MainScreenWindow.setVisible(true);
-        
+
     }
 
 }
