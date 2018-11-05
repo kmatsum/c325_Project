d8b4fab11668369c2cd6f0935d398873d33c546e
PaulTheNarwhal <muellerindy2@yahoo.com>
2018-11-04 20:32:25 -0500

    Added Budget Plan

--- src/c325_project/BudgetPlan.java
+++ src/c325_project/BudgetPlan.java
@@ -10,5 +10,22 @@ package c325_project;
  * @author Paul Mueller
  */
 public class BudgetPlan {
-    
+    private double homeExpenses;
+
+    public void setbudgetPlan(double he) {
+        homeExpenses = he;
+    }
+
+    public double getbudgetPlan() {
+        return homeExpenses;
+        
+    private double transportation;
+
+    public void setuserID(double tp) {
+        transportation = tp;
+    }
+
+    public double getuserID() {
+        return homeExpenses;
+    }
 }
