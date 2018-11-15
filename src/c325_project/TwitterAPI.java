0f99fd8e4acc7aa5d26d1d7d59e86cbab3a71860
Clark462 <44757858+Clark462@users.noreply.github.com>
2018-11-15 12:59:17 -0500

    TwitterAPI 100000000% finished

--- src/c325_project/TwitterAPI.java
+++ src/c325_project/TwitterAPI.java
@@ -9,11 +9,16 @@ package c325_project;
  *
  * @author Jon
  */
+import java.io.PrintStream;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import twitter4j.Status;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
+
 /**
  *
  * @author Jon
@@ -23,25 +28,28 @@ public class TwitterAPI {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws TwitterException {
+    public void ShowTweets() throws TwitterException {
         ConfigurationBuilder cb = new ConfigurationBuilder();
-        
+
         cb.setDebugEnabled(true)
                 .setOAuthConsumerKey("iARj2yVmPwMMHaEo9PJgc9R60")
                 .setOAuthConsumerSecret("ATeQfjwGt0GGy2tbttXguIFkRe7H7OZC7RcpPiPH9LaNwzDsdJ")
                 .setOAuthAccessToken("1059604074994249729-CffFKaWxv4cgD80gBpSIRkNhpdkdjy")
                 .setOAuthAccessTokenSecret("hE4V8KmJrpf8gdDBus63hZYDIYdSMYhRHFIE6rAabh6MI");
-        
+
         TwitterFactory tf = new TwitterFactory(cb.build());
         twitter4j.Twitter twitter = tf.getInstance();
-        
+
         //get username, status
-        
-       List<Status> status = twitter.getHomeTimeline();
-       for(Status st : status){
-           System.out.println(st.getUser().getName()+"------"+st.getText());
-       }
-        
+        List<Status> status = twitter.getHomeTimeline();
+        for (Status st : status) {
+            System.out.println(st.getUser().getName() + "------" + st.getText());
+        }
     }
-    
+
+    public static void main(String[] args) throws TwitterException {
+        TwitterAPI T1 = new TwitterAPI();
+        T1.ShowTweets();
+
+    }
 }
